946636290ef964c3964ac057ca3cc5c0fb03ec35
Gautham <gautham.m@vyoog.com>
2025-02-27 11:45:11 +0530

    lead commit

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -25,6 +25,8 @@ public class LoginSteps {
             // Run in headless mode only in Jenkins
             if (System.getenv("JENKINS_HOME") != null) {
                 options.setHeadless(true);
+                options.addArguments("--headless");
+
                 options.addArguments("--no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
             }
